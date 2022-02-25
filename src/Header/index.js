db38fe3d485476ecfda4243ca752c6a17ccfd701
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2022-02-25 14:56:51 +0700

    change time

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,19 +1,25 @@
 import styled from "@emotion/styled";
 import { Button, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
-import React from "react";
+import React, { useContext } from "react";
+import settingContext from "../SettingContext";
+
 const useStyles = makeStyles({
   header: {
     display: "flex",
     justifyContent: "space-between",
-    maxWidth: 480,
+    height: 60,
+    width: "100%",
     margin: "auto",
-    marginBottom: 20,
-    paddingTop: 20,
     alignItems: "center",
   },
   buttonBar: {
     alignItems: "center",
+  },
+  headerContainer: {
+    padding: "0px 12px",
+    maxWidth: 620,
+    margin: "auto",
   },
 });
 
@@ -27,19 +33,27 @@ const StyledButton = styled(Button)(() => ({
   },
 }));
 const Header = () => {
+  const settingInfo = useContext(settingContext);
   const classes = useStyles();
   return (
-    <div className={classes.header}>
-      <div>
-        <Typography sx={{ fontWeight: "bold", fontSize: 20 }}>
-          Pomofocus
-        </Typography>
-      </div>
+    <div className={classes.headerContainer}>
+      <div className={classes.header}>
+        <div>
+          <Typography sx={{ fontWeight: "bold", fontSize: 20 }}>
+            Pomofocus
+          </Typography>
+        </div>
 
-      <div className={classes.buttonBar}>
-        <StyledButton variant="contained">Report</StyledButton>
-        <StyledButton variant="contained">Setting</StyledButton>
-        <StyledButton variant="contained">Login</StyledButton>
+        <div className={classes.buttonBar}>
+          <StyledButton variant="contained">Report</StyledButton>
+          <StyledButton
+            variant="contained"
+            onClick={() => settingInfo.setShowSetting(true)}
+          >
+            Setting
+          </StyledButton>
+          <StyledButton variant="contained">Login</StyledButton>
+        </div>
       </div>
     </div>
   );
--- src/Main/index.js
+++ src/Main/index.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import Pomodoro from "./Pomo";
+import { makeStyles } from "@mui/styles";
+import Header from "../Header";
+import settingContext from "../SettingContext";
+import Setting from "./Pomo/Setting";
 
+const useStyles = makeStyles(() => ({
+  container: {
+    backgroundColor: "rgb(217,85,80)",
+    minHeight: "100vh",
+    width: "100%",
+    color: "white",
+    transition: "background-color 0.5s ease-in-out 0s",
+    paddingBottom: 12,
+    boxSizing: "border-box",
+  },
+}));
 const HomePage = () => {
+  const classes = useStyles();
+  const settingInfo = useContext(settingContext);
   return (
-    <div style={{ textAlign: "center" }}>
+    <div className={classes.container}>
+      <Header />
       <Pomodoro />
+      {settingInfo.showSetting && <Setting />}
     </div>
   );
 };
